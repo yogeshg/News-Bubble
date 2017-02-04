852a6c6bf75e073617ebeb3e7770dea0df0b6301
Yogesh Garg <yogeshg91@gmail.com>
2017-02-03 21:18:17 -0500

    minimal product

---
+++ NewsBubble.py
@@ -0,0 +1,88 @@
+import time
+import json
+import requests
+import re
+import nltk
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from PIL import Image
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+
+
+import nytimesarticle as nyt
+
+import Util
+
+API_QUERY_RATE = 5 # per second
+NYT_CACHE = './nyt.cache'
+
+class NytApi():
+    def __init__(self, accountKey=None):
+        if( accountKey==None ):
+            with open('APIKEY.txt', 'r') as f:
+                accountKey = f.readline().strip()
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.info(self.__class__.__name__+'Initialised')
+        self.cache = Util.FileDict(NYT_CACHE)
+        self.api = nyt.articleAPI(accountKey)
+        self.API_FIELDS = ['headline', 'pub_date', 'abstract', 'snippet', 'lead_paragraph']
+
+    def search(self, date, query_word='president', num_pages=5):
+        cache_key = str( (date, query_word, num_pages) )
+        if(not self.cache.has_key(cache_key)):
+            all_articles = []
+            for i in range(num_pages):
+                result = self.api.search(  q=query_word,
+                            begin_date=date,
+                            fl=self.API_FIELDS,
+                            sort='oldest',
+                            page=i )
+                time.sleep(4 * 1.0/API_QUERY_RATE)
+                self._last_result = result
+                try:
+                    all_articles.extend(result['response']['docs'])
+                except Exception, e:
+                    self.logger.exception(e)
+            self.cache[cache_key] = all_articles
+        return self.cache[cache_key]
+
+PS = nltk.stem.PorterStemmer()
+NORMALIZE = lambda w: PS.stem_word(w.strip()).lower()
+STOPWORDS = [NORMALIZE(w) for w in open('stopwords.txt','r')]
+
+class NewsBubble():
+    def __init__(self):
+        self.a = NytApi()
+        self.wcf = WordCloud(stopwords=STOPWORDS)
+
+        self.cache = {}
+
+        self.text = []
+        self.words = []
+
+    def getWords(self,sd, ed, query_word, num_pages=5):
+        cache_key = str((sd, ed, query_word, num_pages))
+        if(not self.cache.has_key(cache_key)):
+            all_articles = []
+            for d in range(sd, ed):
+                all_articles.extend(self.a.search(d, query_word, num_pages))
+            text = [article['headline']['main'] for article in all_articles]
+            words = re.findall('\w+', '. '.join(text))
+            stems = [NORMALIZE(w) for w in words]
+            stem2word = dict(zip(stems, words))
+            words2 = map(lambda x: stem2word[x], stems)
+            self.cache[cache_key] = words2
+        return self.cache[cache_key]
+
+    def makeCloud(self,sd, ed, query_word, num_pages=5, save=True):
+        words = self.getWords(sd, ed, query_word, num_pages)
+        wc = self.wcf.generate(' '.join(words))
+        wci = wc.to_image()
+        if(save):
+            cache_key = str((sd, ed, query_word, num_pages))
+            wci.save(Util.fsSafeString(cache_key)+'.png', format='png')
+        return wci
+
---
+++ Util.py
@@ -0,0 +1,66 @@
+import json
+
+import os
+import errno
+
+import re
+
+WORD_REGEX = re.compile(r'[\w]+')
+def fsSafeString( text ):
+    return "_".join(WORD_REGEX.findall(text))
+
+def ensureDir(path):
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
+class FileDict(dict):
+    def __init__(self, filename):
+        self.filename = filename
+        self.reload()
+
+    def reload(self):
+        try:
+            with open(self.filename, 'r') as f:
+                d = json.load(f)
+                dict.__init__(self, d)
+        except:
+            pass
+        return
+
+    def __setitem__(self, i, y):
+        r = dict.__setitem__(self, i, y)
+        with open(self.filename, 'w') as f:
+            json.dump(self, f, sort_keys=True, indent=2)
+        return r
+
+    def __getitem__(self, i):
+        return dict.__getitem__(self, i)
+
+class DirDict(FileDict):
+    def __init__(self, dirname, keyHasher=fsSafeString):
+        ensureDir(dirname)
+        self.dirname = dirname
+        self.keyHasher = keyHasher
+        FileDict.__init__(self, os.path.join(self.dirname, '.keys2file') )
+        self.reload()
+
+    def filePath(self, filename):
+        return os.path.join(self.dirname, filename)
+
+    def __setitem__(self, key, content):
+        filename = self.keyHasher(key)
+        FileDict.__setitem__( self, key, filename )
+        with open( self.filePath( filename ), 'w' ) as f:
+            for l in content:
+                f.write(l)
+            f.flush()
+
+    def __getitem__(self, key):
+        filename = FileDict.__getitem__(self, key)
+        with open( self.filePath( filename ), 'r' ) as f:
+            r = "".join(f.readlines())
+        return r
+
